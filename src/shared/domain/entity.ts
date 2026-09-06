a6444075daa11cfe9fc7cbc31af99e8f84f48b3a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-09-06 12:18:08 -0300

    refactor(shared): improve readability of equals method in Entity

--- src/shared/domain/entity.ts
+++ src/shared/domain/entity.ts
@@ -8,8 +8,12 @@ export abstract class Entity<T extends UniqueId = UniqueId> {
   }
 
   equals(other: Entity<T>): boolean {
-    if (other === null || other === undefined) return false;
-    if (this === other) return true;
+    if (other === null || other === undefined) {
+      return false;
+    }
+    if (this === other) {
+      return true;
+    }
     return this.id.equals(other.id);
   }
 }
