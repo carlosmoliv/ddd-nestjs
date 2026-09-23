d2ff8537c6eee4dac873fe18afb19cd18df5c4a9
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-09-22 23:08:26 -0300

    feat(product): call command bus to execute the create product command

--- src/product/presentation/product.controller.ts
+++ src/product/presentation/product.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateProductDto } from './dtos/create-product.dto.js';
+import { ProductResponseDto } from './dtos/product-response.dto.js';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateProductCommand } from '../application/use-cases/create-product/create-product.command.js';
 
 @Controller()
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly commandBus: CommandBus) {}
+
+  @Post()
+  async create(@Body() dto: CreateProductDto): Promise<void> {
+    await this.commandBus.execute(
+      new CreateProductCommand(
+        dto.name,
+        dto.description,
+        dto.sku,
+        dto.price,
+        dto.currency,
+        dto.stock,
+      ),
+    );
+  }
+}
