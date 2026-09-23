fbde1ad68de3a7f223ef5d25b4a6bfe5810e0af2
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-09-22 23:10:56 -0300

    feat(product): wire product repository provider in product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ProductController } from './presentation/product.controller.js';
+import { PRODUCT_REPOSITORY } from './application/ports/product.repository.js';
+import { DrizzleProductRepository } from './infra/adapters/drizzle-product.repository.js';
 
 @Module({
   imports: [CqrsModule],
   controllers: [ProductController],
+  providers: [
+    {
+      provide: PRODUCT_REPOSITORY,
+      useClass: DrizzleProductRepository,
+    },
+  ],
 })
 export class ProductModule {}
